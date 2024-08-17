3decf6d30e3691d8b42d0f33bc5aee76ec11143d
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-08-17 02:50:37 +0100

    meta-tags

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -10,6 +13,9 @@ export default {
         // primaryText: "var(--primary-text-color)",
         // secondaryBg2: "var(--secondary-bg-color-2)",
         // disabledColor: 'var(--disabled-gold-color)'
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
